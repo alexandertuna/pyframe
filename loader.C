603bd3310140aa0472f5928973a91e525f3ea3cb
alexandertuna <alexander.tuna@gmail.com>
2014-01-02 02:04:37 -0500

    first port from svn

---
+++ loader.C
@@ -0,0 +1,25 @@
+/*
+ * loader.C - A file for helping PyROOT deal. Kinda black magic.
+ */
+#include<vector>
+#include<string>
+#include<set>
+
+#ifdef __CINT__
+#pragma link C++ class vector<string>;
+#pragma link C++ class vector<vector<string> >;
+#pragma link C++ class vector<vector<int> >;
+#pragma link C++ class vector<vector<unsigned int> >;
+#pragma link C++ class vector<vector<float> >;
+#pragma link C++ class vector<vector<double> >;
+#pragma link C++ class pair<set<int>::iterator,bool>;
+#else
+template class std::vector<std::vector<float> >;
+template class std::vector<std::vector<double> >;
+template class std::vector<std::vector<int> >;
+template class std::vector<std::vector<unsigned int> >;
+template class std::vector<std::vector<long> >;
+template class std::vector<std::vector<unsigned long> >;
+template class std::vector<std::vector<short> >;
+template class std::vector<std::vector<unsigned short> >;
+#endif
